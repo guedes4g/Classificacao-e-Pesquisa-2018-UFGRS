41cc71823c4bd111095b3c2109be041de8bf85c0
guedes4g <guedes4g@hotmail.com>
2018-05-24 15:41:28 -0300

    Commit inicial ja com hashMap

---
+++ cmake-build-debug/CMakeFiles/CPD_FINAL.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPD_FINAL.dir/main.cpp.o"
+  "CMakeFiles/CPD_FINAL.dir/HashMap.cpp.o"
+  "CPD_FINAL.pdb"
+  "CPD_FINAL"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPD_FINAL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <tensorflow/c/c_api.h>
+
+#include "HashMap.cpp"
+
+int main() {
+    printf("Hello from TensorFlow C library version %s\n", TF_Version());
+
+    auto *map = new HashMap<string, unsigned int>(100);
+    cout << "insert 1" << endl;
+    map->insertNode("asa",3);    cout << "insert 2" << endl;
+
+    map->insertNode("aaa",3);    cout << "insert 3" << endl;
+
+    map->insertNode("aba",3);    cout << "insert 4" << endl;
+    map->insertNode("aba",3);    cout << "insert 4" << endl;
+//
+//    map->insertNode("asa",3);    cout << "insert 5" << endl;
+//
+//    map->insertNode("asa",3);    cout << "insert 6" << endl;
+//
+//    map->insertNode("asa",3);    cout << "insert 7" << endl;
+//
+//    map->insertNode("asa",3);    cout << "insert 8" << endl;
+
+    map->set("asa",4);
+    cout << "after set "<< map->get("asa") << endl;
+    cout << map->get("aaa") << endl;
+    map->deleteNode("asa")  ;
+    cout << " -- -- -- --" << endl;
+    map->deleteNode("asa") ;
+    cout << map->get("asa") << endl;
+
+    cout << map->sizeofMap() << endl;
+    printf("END");
+    return 0;
+}
